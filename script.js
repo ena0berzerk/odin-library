12f70bd4afe21201a4cd0684e296f1da35371618
enduraew <enduraew@gmail.com>
2023-09-08 09:16:12 +0500

    Add modal window script
    Impement close btn modal / outside of modal / close by ESC keyboard

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,7 +10,37 @@ Book.prototype.info = function () {
 };
 
 const lotr = new Book('The Lord of The Rings', 'J.J.R Tolkien', '431', 'not read yet');
+const wh40k = new Book('Horus Heresy', 'A lot', '5000', 'read');
+
+const myLibrary = [lotr, wh40k];
 
 function modalNewBookBtn() {
-  const 
+  const addNewBookBtn = document.querySelector('.add-book-btn');
+  const modalWindow = document.querySelector('.modal-window');
+  const closeBtn = document.querySelector('.close');
+
+  addNewBookBtn.addEventListener('click', () => {
+    modalWindow.classList.remove('hidden');
+
+    closeBtn.addEventListener('click', () => {
+      closeModal();
+    });
+
+    window.addEventListener('click', e => {
+      if (e.target === modalWindow) {
+        closeModal();
+      }
+    });
+
+    window.addEventListener('keydown', e => {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    });
+  });
+
+  function closeModal() {
+    return modalWindow.classList.add('hidden');
+  }
 }
+modalNewBookBtn();
