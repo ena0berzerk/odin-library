2695f4aadf332768e5680e996dd6cdf26435c61e
enduraew <enduraew@gmail.com>
2023-09-12 10:55:19 +0500

    Add two function about table and get data
    write function that allow show book that in myLibrary arr
    also implement function that allow us receive data from submit form and convert them to instance class

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
 
-const lotr = new Book('The Lord of The Rings', 'J.J.R Tolkien', '431', 'not read yet');
-const wh40k = new Book('Horus Heresy', 'A lot', '5000', 'read');
+const lotr = new Book('The Lord of The Rings', 'J.J.R Tolkien', '431', 'no');
+const wh40k = new Book('The Horus Heresy', 'Dan Abnett', '6520', 'yes');
 
 const myLibrary = [lotr, wh40k];
 
-function modalNewBookBtn() {
+function showModalNewBook() {
   const addNewBookBtn = document.querySelector('.add-book-btn');
   const modalWindow = document.querySelector('.modal-window');
   const closeBtn = document.querySelector('.close');
@@ -43,4 +43,40 @@ function modalNewBookBtn() {
     return modalWindow.classList.add('hidden');
   }
 }
-modalNewBookBtn();
+showModalNewBook();
+
+function getDataFromForm() {
+  const form = document.querySelector('.modal-form');
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  const pages = document.querySelector('#pages');
+  const read = document.querySelector('#read');
+
+  form.addEventListener('submit', e => {
+    const convertDataToInstantiateObject = new Book(
+      title.value,
+      author.value,
+      pages.value,
+      `${read.checked ? 'yes' : 'no'}`
+    );
+    myLibrary.push(convertDataToInstantiateObject);
+    console.log(myLibrary);
+    e.preventDefault();
+  });
+}
+getDataFromForm();
+
+function showBookInTable() {
+  for (const i in myLibrary) {
+    const table = document.querySelector('table');
+    const tr = document.createElement('tr');
+    table.appendChild(tr);
+    const objValue = Object.values(myLibrary[i]);
+    for (const value in objValue) {
+      const td = document.createElement('td');
+      td.textContent = objValue[value];
+      tr.appendChild(td);
+    }
+  }
+}
+showBookInTable();
